e2a8570548b0ab0e631b38f134b65e19da69b216
Tiago Biazin <tiago.biazin02@gmail.com>
2024-06-27 10:14:28 -0300

    alteração porta servidor

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const app = require('./app');
-const PORT = process.env.PORT || 5000;
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
